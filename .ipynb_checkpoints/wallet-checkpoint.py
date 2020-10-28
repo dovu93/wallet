a5950776a8d302e8b409b172b3b7888b6cc1c075
dovu93 <dovu93@icloud.com>
2020-10-27 21:23:39 -0500

    adding in partial hw

---
+++ .ipynb_checkpoints/wallet-checkpoint.py
@@ -0,0 +1,16 @@
+run constants.py
+
+import subprocess
+import json
+
+command = './derive -g --mnemonic="INSERT HERE" --cols=path,address,privkey,pubkey --format=json'
+
+p = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True)
+output, err = p.communicate()
+p_status = p.wait()
+
+keys = json.loads(output)
+print(keys)
+
+
+./hd-wallet-derive/hd-wallet-derive.php
